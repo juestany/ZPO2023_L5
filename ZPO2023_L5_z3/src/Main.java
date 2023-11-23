7efec9dc3bdd85813c8298a4d82ce6ed9219a536
Justyna Kwak <108225946+juestany@users.noreply.github.com>
2023-11-23 16:10:09 +0100

    update list 6 zad 2

--- ZPO2023_L5_z3/src/Main.java
+++ ZPO2023_L5_z3/src/Main.java
@@ -1,7 +1,19 @@
+enum typ_przedmiotu {
+    WYKLAD,
+    CWICZENIA,
+    LABORATORIA,
+    SEMINARIUM
+}
+
+enum sposob_zaliczenia {
+    EGZAMIN,
+    ZALICZENIE
+}
+
 public class Main {
     public static void main(String[] args) {
         Przedmiot przedmiot1 = new Przedmiot("ABC123", "ZPO",
-                3, 60, 5);
+                3, 60, 5, typ_przedmiotu.LABORATORIA, sposob_zaliczenia.ZALICZENIE);
         Przedmioty listaPrzedmiotow = new Przedmioty();
         listaPrzedmiotow.dodajPrzedmiot(przedmiot1);
         listaPrzedmiotow.przedstawPrzedmiot(przedmiot1);
