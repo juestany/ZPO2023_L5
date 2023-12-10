43ae0a42af1aece617156d0382993c4c9a94d08d
Justyna Kwak <108225946+juestany@users.noreply.github.com>
2023-12-10 18:24:26 +0100

    Update: L7, JSON files

--- ZPO2023_L5_z3/src/Przedmioty.java
+++ ZPO2023_L5_z3/src/Przedmioty.java
@@ -1,6 +1,14 @@
 import java.util.*;
+import java.io.*;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import java.io.File;
+import org.json.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class Przedmioty {
-    public Map<String, Przedmiot> listaPrzedmiotow;
+    public static Map<String, Przedmiot> listaPrzedmiotow;
 
     public Przedmioty () {
         /**
@@ -9,7 +17,88 @@ public class Przedmioty {
         listaPrzedmiotow = new HashMap<String, Przedmiot>();
     }
 
-    public void dodajPrzedmiot(Przedmiot przedmiot) {
+    public static JSONObject PrzedmiotyJakoJSON(Przedmioty przedmioty) {
+        /**
+         * Metoda zapisująca obiekt klasy Przedmioty w formie JSON.
+         * @param przedmioty (Przedmioty)
+         * @returns jsonObject(JSONObject)
+         */
+        JSONObject jsonObject = new JSONObject();
+        for(Map.Entry<String, Przedmiot> obiektPrzedmiot : przedmioty.listaPrzedmiotow.entrySet()) {
+            // iteracja po kolejnych przedmiotach
+            JSONObject przedmiotJSON = new JSONObject();
+            Przedmiot przedmiot = obiektPrzedmiot.getValue();
+
+            przedmiotJSON.put("kod_przedmiotu", przedmiot.getKod_przedmiotu());
+            przedmiotJSON.put("nazwa_przedmiotu", przedmiot.getNazwa_przedmiotu());
+            przedmiotJSON.put("ECTS", przedmiot.getECTS());
+            przedmiotJSON.put("wymiar_godz", przedmiot.getWymiar_godz());
+            przedmiotJSON.put("semestr", przedmiot.getSemestr());
+            przedmiotJSON.put("typ_przedmiotu", przedmiot.getTyp_przedmiotu().toString());
+            przedmiotJSON.put("sposob_zaliczenia", przedmiot.getSposob_zaliczenia().toString());
+
+            if (jsonObject.containsKey(przedmiot.getKod_przedmiotu())) { // jeśli taki klucz już jest
+                JSONArray przedmiotArray = (JSONArray) jsonObject.get(przedmiot.getKod_przedmiotu());
+                przedmiotArray.add(przedmiotJSON);
+            } else { // jeśli nie, to stwórz nowy
+                JSONArray przedmiotArray = new JSONArray();
+                przedmiotArray.add(przedmiotJSON);
+                jsonObject.put(przedmiot.getKod_przedmiotu(), przedmiotArray);
+            }
+        }
+        return jsonObject;
+    }
+
+    public static void eksportJSON(JSONObject jsonObject, String nazwaPliku) {
+        /**
+         * Metoda pozwalająca zapisać obiekt JSON w formie pliku.
+         * @param jsonObject (JSONObject)
+         * @param nazwaPliku (String)
+         */
+        try {
+            FileWriter file = new FileWriter(nazwaPliku);
+            file.write(jsonObject.toJSONString());
+            file.close();
+        } catch (IOException e) {
+            e.getMessage();
+        }
+        System.out.println("JSON file created: "+ jsonObject);
+    }
+
+    public static Przedmioty importJSON(String path) throws IOException, ParseException {
+        /**
+         * Metoda pozwalająca na wczytanie pliku JSON z listą przedmiotów i zwrócenie jej w postaci klasy Przedmioty.
+         * @param path (String)
+         * @returns przedmioty (Przedmioty)
+         * @throws IOException, ParseException
+         */
+        Przedmioty przedmioty = new Przedmioty();
+        try {
+            JSONParser jsonParser = new JSONParser();
+            JSONObject jsonObject = (JSONObject) jsonParser.parse(new FileReader(path));
+
+            for (Object key : jsonObject.keySet()) {
+                String kodPrzedmiotu = (String) key;
+                JSONObject przedmiotJson = (JSONObject) jsonObject.get(kodPrzedmiotu);
+                
+                String nazwa_przedmiotu = (String) przedmiotJson.get("nazwa_przedmiotu");
+                int ECTS = (int) przedmiotJson.get("ECTS");
+                int wymiar_godz = (int) przedmiotJson.get("wymiar_godz");
+                int semestr = (int) przedmiotJson.get("semestr");
+                typ_przedmiotu typ_przedmiotu = (typ_przedmiotu) przedmiotJson.get("typ_przedmiotu");
+                sposob_zaliczenia sposob_zaliczenia = (sposob_zaliczenia) przedmiotJson.get("sposob_zaliczenia");
+                
+                Przedmiot przedmiot = new Przedmiot(kodPrzedmiotu, nazwa_przedmiotu, ECTS, wymiar_godz, semestr, typ_przedmiotu, sposob_zaliczenia);
+                przedmioty.dodajPrzedmiot(przedmiot);
+            }
+
+        } catch (IOException | ParseException e) {
+            e.getMessage();
+        }
+        return przedmioty;
+    }
+
+    public static void dodajPrzedmiot(Przedmiot przedmiot) {
         /**
          * Metoda pozwalająca na dodanie przedmiotu do listy przedmiotów.
          * @param przedmiot (Przedmiot)
@@ -47,7 +136,7 @@ public class Przedmioty {
             System.out.println("Brak przedmiotów do podliczenia.");
             return;
         }
-        
+
         int suma_godzin = this.listaPrzedmiotow
                 .values()
                 .stream()
@@ -62,6 +151,7 @@ public class Przedmioty {
                 .mapToInt(Przedmiot::getECTS)
                 .sum();
 
-        System.out.println("Suma godzin dla danego typu przedmiotu: " + suma_godzin + "\nSuma ECTS dla danego przedmiotu: " + suma_ECTS);
+        System.out.println("Suma godzin dla danego typu przedmiotu: " + suma_godzin + "\nSuma ECTS dla danego " +
+                "przedmiotu: " + suma_ECTS);
     }
 }
